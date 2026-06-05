55db6e62286704b508db5e091805da0fb8b5ca41
Philippe Fanaro <philippefanaro@gmail.com>
2026-06-05 12:56:29 +0900

    trying to implement the navbar

---
+++ app/cursos/page.tsx
@@ -0,0 +1,18 @@
+export default function About() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+          Olá, meu nome é Philippe Fanaro e sou um instrutor de Go. Tenho mais
+          de 10 anos de experiência em desenvolvimento de software e ensino Go
+          há mais de 5 anos. Meus cursos são projetados para ajudar
+          desenvolvedores a aprender Go de forma rápida e eficiente, com foco em
+          práticas recomendadas e exemplos do mundo real. Se você está
+          interessado em aprender Go, confira meus cursos e comece sua jornada
+          hoje mesmo!
+        </h1>
+        <p></p>
+      </main>
+    </div>
+  )
+}
